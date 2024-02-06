cc394430656aa1aeb514a8fe83c872705b742d31
Devanshu M <devansm@microsoft.com>
2024-02-07 00:16:51 +0530

    Added one happy path test case

--- src/test/java/org/codapayments/echo/EchoApplicationTests.java
+++ src/test/java/org/codapayments/echo/EchoApplicationTests.java
@@ -1,13 +1,39 @@
 package org.codapayments.echo;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EchoController.class)
 class EchoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private EchoController controller;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    void testHappyPath() throws Exception {
+        String payloadString = "{\"game\":\"Mobile Legends\", \"gamerID\":\"GYUTDTE\", \"points\":20}";
+        JSONObject payload = new JSONObject(payloadString);
+        this.mockMvc
+                .perform(post("/echo").contentType(MediaType.APPLICATION_JSON).content(payload.toString()))
+                .andExpect(status().isOk()).andExpect(content().string(payload.toString()));
+    }
 
 }
